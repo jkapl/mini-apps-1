9b0c291be4a3a8041469de33bcc0f23726f26099
Joel <joelkaplan1@gmail.com>
2019-06-24 15:05:19 -0700

    fix reset bug, vert and horiz checks working

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,45 +3,65 @@ let players = {
   'Player 1' : true
 }
 let squares = document.querySelectorAll('td');
-
 let resetButton = document.getElementById('reset')
 resetButton.addEventListener('click', ()=> {
   reset();
 });
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', (event)=> {
+    addToe(event);
+  })
+}
 
 const reset = () => {
-  currentPlayer = 'Player 1';
-
+  players['Player 1'] = true;
   for (let i = 0; i < squares.length; i++) {
-    squares[i].innerText = '[  ]'
-    squares[i].addEventListener('click', (event)=> {
-      // event.currentTarget.innerText = '[ x ]'
-      addToe(event);
-    })
+    squares[i].innerText = '[    ]' 
   };
 };
 reset();
 
 //check methods
 const verticalCheck = () => {
-
+  let emptySq = '[ ]'
+  for (var i = 0; i < 3; i++) {
+    if ( (squares[i].innerText === squares[i+3].innerText) && 
+    squares[i+3].innerText && squares[i+6].innerText && 
+    ( squares[i].innerText !== emptySq && squares[i+3].innerText!== emptySq && squares[i+6].innerText!== emptySq)) {
+      console.log('vertical win')
+    }
+  }
 }
 
 const horizCheck = () => {
-
+  let emptySq = '[ ]'
+  for (var i = 0; i < 3; i++) {
+    if ( (squares[i].innerText === squares[i+1].innerText ) && 
+    (squares[i+1].innerText === squares[i+2].innerText) && 
+    ( squares[i].innerText !== emptySq && squares[i+1].innerText !== emptySq && squares[i+3]!== emptySq)) {
+      console.log('horiz win')
+    }
+  }
 }
 
 const diagCheck = () => {
+  for (var i = 0; i < 2; i++) {
+    
+  }
 
 }
 
 //play methods
 const addToe = (event) => {
-  if (players["Player 1"]) {
+  console.log(players);
+  if (players['Player 1']) {
     event.currentTarget.innerText = '[  x  ]'
   } else {
     event.currentTarget.innerText = '[  o  ]'
   }
   players['Player 1'] = !players['Player 1'];
+  console.log(players['Player 1'] + ' after')
+  horizCheck();
+  verticalCheck();
 }
 
