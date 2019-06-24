ad0a6f00be90ec3f34a224a05a9e8c359d863c30
Joel <joelkaplan1@gmail.com>
2019-06-24 14:06:57 -0700

    create board, create outline for app.js

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,31 @@
+// initialization
+let currentPlayer = 'Player 1';
+
+let resetButton = document.getElementById('reset')
+resetButton.addEventListener('click', ()=> {
+  console.log('clicked');
+});
+
+const reset = () => {
+  let board = document.getElementById('board');
+  currentPlayer = 'Player 1';
+  
+};
+
+
+//check methods
+const verticalCheck = () => {
+
+}
+
+const horizCheck = () => {
+
+}
+
+const diagCheck = () => {
+
+}
+
+//play methods
 
 
