d34b9b23bdd3b3ff68b3bd7035e04a3ebfa3e911
Joel <joelkaplan1@gmail.com>
2019-06-26 21:02:14 -0700

    begin to install mongodb

---
+++ challenge_3/db/schema.js
@@ -0,0 +1,11 @@
+// import connection from 'index.js';
+
+// const Schema = connection.Schema;
+// const ObjectId = Schema.ObjectId;
+ 
+// const BlogPost = new Schema({
+//   author: ObjectId,
+//   title: String,
+//   body: String,
+//   date: Date
+// });
